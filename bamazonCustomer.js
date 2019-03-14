01d538ec8da827c3c1f908c0514b6922f8ca6c36
gladyn <glds.nvrr@gmail.com>
2019-03-13 18:16:31 -0700

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,16 +17,97 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+connection.connect(function (err) {
+    if (err) throw err;
 });
 
-//functionn promting the user
-function start () {
-    inquirer
-    .prompt({
-        
+function userPurchase() {
+
+    // Prompt the user to select an item
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter the Item ID which you would like to purchase.',
+            filter: Number
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: 'How many do you need?',
+            filter: Number
+        }
+    ]).then(function (input) {
+
+        var item = input.id;
+        var quantity = input.quantity;
+        var queryStr = 'SELECT * FROM products WHERE ?';
+
+        connection.query(queryStr, { id: item }, function (err, res) {
+            if (err) throw err;
+            // console.table(res);
+            if (res.length === 0) {
+                console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+                displayInventory();
+
+            } else {
+                var productData = res[0];
+
+                // If the quantity requested by the user is in stock
+                if (quantity <= productData.stock_quantity) {
+                    console.log('\nCongratulations, your item is in stock!\n');
+
+                    // Construct the updating query string
+                    var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE id = ' + item;
+
+                    // Update the inventory
+                    connection.query(updateQueryStr, function (err, res) {
+                        if (err) throw err;
+
+                        // console.table(res);
+                        console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+                        console.log('\nPlease shop with us again soon!\n');
+                        console.log("\n---------------------------------------------------------------------\n");
+                        // // console.table(res);
+                        // End the database connection
+                        connection.end();
+                    })
+                } else {
+                    console.log('\nSorry, there is not enough in stock, your order can not be placed as is.\n');
+                    console.log('Please modify your order.');
+                    console.log("\n---------------------------------------------------------------------\n");
+                    // console.log("Please enter a new item ID");
+                    userPurchase();
+                    // displayInventory();
+                }
+            }
+        })
     })
 }
+
+// displayInventory will retrieve the current inventory from the database and output it to the console
+function displayInventory() {
+
+    queryStr = 'SELECT * FROM products';
+
+    // Make the db query
+    connection.query(queryStr, function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+
+        //Prompt the user for item/quantity they would like to purchase
+        userPurchase();
+
+    })
+}
+
+// runBamazon will execute the main application logic
+function runBamazon() {
+
+    // Display the available inventory
+    displayInventory();
+}
+
+// Run the application logic
+runBamazon();
--- bamazonCustomerSchema.sql
+++ bamazonCustomerSchema.sql
@@ -8,7 +8,7 @@ CREATE TABLE products (
 item_id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(60) NOT NULL,
 department_name VARCHAR(60) NOT NULL,
-price DECIMAL(10,3) NULL,
+price DECIMAL(10,2) NULL,
 stock_quantity INT NOT NULL,
 PRIMARY KEY(item_id)
 );
